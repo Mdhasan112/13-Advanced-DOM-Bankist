2f3bcd961987895163af56936b6f229512ad25a8
hasan <mdhasanhk.98@gmail.com>
2023-03-04 17:27:50 +0600

    Event Propagation in Practice

--- starter/script.js
+++ starter/script.js
@@ -174,3 +174,33 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
 // };
 ///////////////////////////////////////
 */
+
+/*
+///////////////////////////////////////
+//rgb(255, 255, 255);
+const randomInt = (max, min) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // //Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
+///////////////////////////////////////
+*/
