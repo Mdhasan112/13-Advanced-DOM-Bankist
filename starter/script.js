abb453fa980c76a27cacb35aaf22693e7d2858ab
hasan <mdhasanhk.98@gmail.com>
2023-03-05 14:08:51 +0600

    DOM Traversing

--- starter/script.js
+++ starter/script.js
@@ -226,3 +226,30 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 });
 ///////////////////////////////////////
 */
+
+/*
+///////////////////////////////////////
+const h1 = document.querySelector('h1');
+
+//Going downwards: child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+console.log('------------------------------');
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+h1.closest('.header').style.backgroundColor = 'var(--color-tertiary-opacity)';
+
+//Going sideways: sibling
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+console.log(h1.parentElement.children);
+///////////////////////////////////////
+*/
